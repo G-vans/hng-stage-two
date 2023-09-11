5fc638827416929bbc5212044b99621030ff8289
Jevans <jevansstanley@gmail.com>
2023-09-11 14:42:50 +0300

    fix: add validations to age input

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ApplicationRecord
     validates :name, format: { with: /\A[a-zA-Z\s]+\z/, message: "Only letters are allowed" }
     validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Invalid email format" }
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   end
   
